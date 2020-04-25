445cf053f00256f2e62f442565b91ed0f15a05e1
JohnTheBeloved <topzy20@gmail.com>
2020-04-25 11:48:05 +0100

    Develop branch_ odoo12

---
+++ addons_mine/theme_eiser/__manifest__.py
@@ -0,0 +1,18 @@
+{
+  'name':'Eiser theme',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Your name',
+
+  'data': [
+    'views/layout.xml',
+    'views/pages.xml',
+    'views/assets.xml',
+    'views/snippets.xml',
+    'views/templates.xml',
+    'views/options.xml',
+    'views/header.xml'
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_blog', 'sale'],
+}
